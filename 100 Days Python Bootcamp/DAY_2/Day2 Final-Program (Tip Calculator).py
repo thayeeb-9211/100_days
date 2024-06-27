46524f7df595385f0a4779b8861d8bb5e5f616a6
mohammed thayeeb shariff <devgenius9211@gmail.com>
2024-06-27 22:22:11 +0530

    git commit -m "commit"

--- 100 Days Python Bootcamp/DAY_2/Day2 Final-Program (Tip Calculator).py
+++ 100 Days Python Bootcamp/DAY_2/Day2 Final-Program (Tip Calculator).py
@@ -7,14 +7,25 @@
 
 #Write your code below this line 👇
 
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+# people = int(input("How many people to split the bill?"))
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
 
-print(f"each person should pay:${final_amount}")
+# print(f"each person should pay:${final_amount}")
+
+while True:
+    print("\nWelcome to the tip calculator...!")
+    bill  = float(input("what was the total bill?$: \n"))
+    tip  = int(input("How much tip would you like to give? 10, 12 or 15 $: \n"))
+    people = int(input("How many people to spit the bill?: \n"))
+  
+    total_bill = bill + (bill * (tip / 100))
+    final_bill  = round((total_bill/people), 2)
+  
+    print(f"bill_per_person: {final_bill}")
