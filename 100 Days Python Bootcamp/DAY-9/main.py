f505932f28cf41db8b9dcd90b793ee6edd370820
mohammed thayeeb shariff <devgenius9211@gmail.com>
2024-06-27 19:37:52 +0530

    sixth

--- 100 Days Python Bootcamp/DAY-9/main.py
+++ 100 Days Python Bootcamp/DAY-9/main.py
@@ -1,29 +1,30 @@
-import clear
-from art import logo
+# import clear
+# from art import logo
 
-print(logo)
-bids={}
-taking_bids = True
-print("Welcome to the secret auction program.")
+# print(logo)
+# bids={}
+# taking_bids = True
+# print("Welcome to the secret auction program.")
 
-while taking_bids:
-  users = input("What is your name?:")
-  bid = input("What is your Bid?:$ ")
+# while taking_bids:
+#   users = input("What is your name?:")
+#   bid = input("What is your Bid?:$ ")
 
-  bids[users] = bid
-  more_bidders = input("Are there any other bidders? Type 'y' or 'n'.")
-  
-  if more_bidders == 'n':
-    taking_bids = False
-  else:
-    clear()
+#   bids[users] = bid
+#   more_bidders = input("Are there any other bidders? Type 'y' or 'n' \n")
 
-winner = next(iter(bids)) # Initialize winner to the first bidder in the dictionary
-highest_bid = 0
-for bidder in bids:
-  bid_amount = int(bids[bidder])
-  if bid_amount > highest_bid:
-    highest_bid = bid_amount
-    winner = bidder
-print(f"The winner is {winner} with a bid of ${highest_bid}")
-  
+#   if more_bidders == 'n':
+#     taking_bids = False
+#   else:
+#     print("\n")
+# winner = next(iter(bids)) 
+# for bid, bidder_value in bids.items():
+#   print(f"[{bid}: {bidder_value}]")
+# highest_bid = 0
+# for bidder in bids:
+#   bid_amount = int(bids[bidder])
+#   if bid_amount > highest_bid:
+#     highest_bid = bid_amount
+#     winner = bidder
+# print(f"The winner is {winner} with a bid of ${highest_bid}")
+
